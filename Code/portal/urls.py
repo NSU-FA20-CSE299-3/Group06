e56d1ff2fe83edb6876ec2b8cf87aff1e3ee9a10
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2020-12-20 01:41:54 +0600

    Created Layout of the portal: #14
    Successfully created the academic, admission, gallery, employment, about us, and contact section, which are linked with portal.

--- Code/portal/urls.py
+++ Code/portal/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-  path("",views.index, name="App Login")
+  path("",views.index, name="App Login"),
+  path("academic/",views.academic,name="Academic"),
+  path("admission/",views.admission,name="Admission"),
+  path("employment/",views.employment,name="Employment"),
+  path("gallery/",views.gallery,name="Gallery"),
+  path("about/",views.about,name="About Us"),
+  path("contact/",views.contact,name="Contact"),
+
+
 ]
--- Code/portal/views.py
+++ Code/portal/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+
 
 # Create your views here.
+from django.http import HttpResponse
 def index (request):
      return render(request,'portal/home.html')
+def academic(request):
+     return HttpResponse("Welcome to academic activities")
+def admission(request):
+     return HttpResponse("Welcome to admission activities")
+def employment(request):
+     return HttpResponse("Welcome to academic section")
+def gallery(request):
+     return HttpResponse("Welcome to portal gallery")
+def about(request):
+     return HttpResponse("Welcome to About Section")
+def contact(request):
+     return HttpResponse("Welcome to Contact Page ")
