c99b1090b9bf759d100a5b57bf8a2c01bbcd5c5b
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2020-12-31 22:05:14 +0600

    Enhancement: #18
    We added file upload section in academic part, By using this teachers can also share large file with the students.

--- Code/portal/models.py
+++ Code/portal/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Academic(models.Model):
     academic_id = models.AutoField
-    academic_name = models.CharField(max_length=50)
-    desc = models.CharField (max_length=300)
-    pub_date = models.DateField() 
+    academic_name = models.CharField(max_length=200)
+    desc = models.TextField ()
+    fileupload = models.FileField(upload_to="portal/static/Documents",default="")
+    pub_date = models.DateField()
+
