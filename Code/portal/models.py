aa7449ac4af61bd8c4aa38cc967e15956bcb8a73
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2021-01-04 04:16:32 +0600

    Enhancement: #21
    We successfully showing the academic info that we stored in the database.
    
    later we will develop this section.

--- Code/portal/models.py
+++ Code/portal/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class Academic(models.Model):
     academic_id = models.AutoField
     academic_name = models.TextField()
-    desc = models.TextField ()
+    desc = models.TextField()
     fileupload = models.FileField(upload_to="portal/static/Documents", default="")
     pub_date = models.DateField()
 
     def __str__(self):
         return self.academic_name
-
--- Code/portal/views.py
+++ Code/portal/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render
 
-
 # Create your views here.
 from django.http import HttpResponse
-def index (request):
-     return render(request, 'portal/home.html')
-def academic(request):
-     return HttpResponse("Welcome to academic activities")
-def admission(request):
-     return HttpResponse("Welcome to admission activities")
-def employment(request):
-     return HttpResponse("Welcome to academic section")
-def gallery(request):
-     return HttpResponse("Welcome to portal gallery")
-def about(request):
 
-     return render(request, 'portal/about.html')
+from portal.models import Academic
+
+
+def index(request):
+    return render(request, 'portal/home.html')
+
+
+def academic(request):
+    academics = Academic.objects.all()
+    print(academics)
+    params = {'academic': academics}
+    return render(request, 'portal/academic.html', params)
+
+
+def admission(request):
+    return HttpResponse("Welcome to admission activities")
+
+
+def employment(request):
+    return HttpResponse("Welcome to academic section")
+
+
+def gallery(request):
+    return HttpResponse("Welcome to portal gallery")
+
+
+def about(request):
+    return render(request, 'portal/about.html')
+
+
 def contact(request):
-     return HttpResponse("Welcome to Contact Page ")
+    return HttpResponse("Welcome to Contact Page ")
