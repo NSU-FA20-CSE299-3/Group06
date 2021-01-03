c477aca3960d95d6fdf5b2fb65f44fcdda43b079
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2021-01-03 23:17:21 +0600

    Enhancement: #19
    We created our About section, which is known as "আমাদের কথা" in our portal.
    we also connected the page with portal section.

--- Code/portal/views.py
+++ Code/portal/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index (request):
-     return render(request,'portal/home.html')
+     return render(request, 'portal/home.html')
 def academic(request):
      return HttpResponse("Welcome to academic activities")
 def admission(request):
@@ -15,6 +15,6 @@ def gallery(request):
      return HttpResponse("Welcome to portal gallery")
 def about(request):
 
-     return HttpResponse("Welcome to About Section")
+     return render(request, 'portal/about.html')
 def contact(request):
      return HttpResponse("Welcome to Contact Page ")
