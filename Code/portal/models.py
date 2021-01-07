53607e557a1ea2b008bbbc7edd35c93bc33f8f96
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2021-01-08 01:07:08 +0600

    Enhancement: #10
    Create the html files of syllabus inside the academic.

--- Code/portal/models.py
+++ Code/portal/models.py
@@ -12,13 +12,11 @@ class Academic(models.Model):
     def __str__(self):
         return self.academic_name
 
-    class Syllabus(models.Model):
-        syllabus_id = models.AutoField
-        syllabus_name = models.TextField()
-        desc = models.TextField()
-        fileupload = models.FileField(upload_to="portal/static/Documents", default="")
-        pub_date = models.DateField()
 
 
-
-
+class Syllabus(models.Model):
+    syllabus_id = models.AutoField
+    syllabus_name = models.TextField()
+    desc = models.TextField()
+    fileupload = models.FileField(upload_to="portal/static/Documents", default="")
+    pub_date = models.DateField()
