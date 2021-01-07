be128bf8afb18e81bbbe0a8450d21938009bcc66
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2021-01-08 00:38:35 +0600

    Enhancement: #10
    Create the model of syllabus inside academic section.

--- Code/portal/models.py
+++ Code/portal/models.py
@@ -11,3 +11,14 @@ class Academic(models.Model):
 
     def __str__(self):
         return self.academic_name
+
+    class Syllabus(models.Model):
+        syllabus_id = models.AutoField
+        syllabus_name = models.TextField()
+        desc = models.TextField()
+        fileupload = models.FileField(upload_to="portal/static/Documents", default="")
+        pub_date = models.DateField()
+
+
+
+
