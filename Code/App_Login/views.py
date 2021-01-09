c7ef42005943b835262ea8fcdd78abf30ed06dbf
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2021-01-09 20:18:08 +0600

    Enhancement: #5
    Trying to create login page for portal for teachers.

--- Code/App_Login/views.py
+++ Code/App_Login/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import HttpResponse
+
 
 # Create your views here.
-def index (request):
-     return render(request,'App_Login/index.html')
+def index(request):
+    return render(request, 'App_Login/index.html')
+
+
+def login_page(request):
+    return HttpResponse("Welcome to Portal")
+
+
+def logout_page(request):
+    return HttpResponse("Welcome to Portal")
