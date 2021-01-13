45fb5ceb9d9c05d9a97821d8ca4e5aeb23d0f068
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2021-01-13 23:53:10 +0600

    Enhancement: #8
    Added class routine inside academic section.

--- Code/portal/urls.py
+++ Code/portal/urls.py
@@ -4,13 +4,14 @@ from. import views
 
 urlpatterns = [
   path("",views.index, name="App Login"),
-  path("academic/",views.academic,name="Academic"),
-  path("academic/syllabus",views.syllabus,name="Syllabus"),
-  path("admission/",views.admission,name="Admission"),
-  path("employment/",views.employment,name="Employment"),
-  path("gallery/",views.gallery,name="Gallery"),
-  path("about/",views.about,name="About Us"),
-  path("contact/",views.contact,name="Contact"),
+  path("academic/", views.academic, name="Academic"),
+  path("academic/class_routine", views.class_routine, name="class_routine"),
+  path("academic/syllabus", views.syllabus, name="Syllabus"),
+  path("admission/", views.admission,name="Admission"),
+  path("employment/", views.employment, name="Employment"),
+  path("gallery/", views.gallery, name="Gallery"),
+  path("about/", views.about, name="About Us"),
+  path("contact/", views.contact, name="Contact"),
 
 
 ]
