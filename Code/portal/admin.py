e8ad338b3fc8421ca1bae2c7c8c5e3cf03033916
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2021-01-13 23:57:00 +0600

    Enhancement: #8
    Registered the model of class routine inside admin panel.

--- Code/portal/admin.py
+++ Code/portal/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Academic
+from .models import class_routine
 from .models import Syllabus
 from .models import Employment
 
-admin.site.register(Academic)
+admin.site.register(class_routine)
 admin.site.register(Syllabus)
 admin.site.register(Employment)
