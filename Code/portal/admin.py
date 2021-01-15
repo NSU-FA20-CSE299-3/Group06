6283d851c101010e3d082b42b4f22398c840bc6c
Fatin Fuad Karim <56217090+fatinfuadkarim@users.noreply.github.com>
2021-01-16 02:28:40 +0600

    Enhancement: #19
    register about section inside admin panel.

--- Code/portal/admin.py
+++ Code/portal/admin.py
@@ -4,7 +4,9 @@ from django.contrib import admin
 from .models import Class_routine
 from .models import Syllabus
 from .models import Employment
+from .models import About
 
 admin.site.register(Class_routine)
 admin.site.register(Syllabus)
 admin.site.register(Employment)
+admin.site.register(About)
